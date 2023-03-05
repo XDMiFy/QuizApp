733611dfd65eb7f1e8d83a9c5f842f45d06880a9
MiFy <r.bakman@icloud.com>
2023-03-05 15:30:11 +0600

    uhh
    OH HELL NAAH

---
+++ app/src/main/java/com/MiFy/Buzz/Login/LoginFragment.kt
@@ -0,0 +1,29 @@
+package com.MiFy.Buzz.Login
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import com.MiFy.Buzz.R
+
+class LoginFragment: Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val button = view.findViewById<Button>(R.id.login_btn)
+        button.setOnClickListener {
+            println("Welcome to hell >:D")
+        }
+
+    }
+}
