9edf4219d45435b1a3967a2b39333b1a8704dc86
MiFy <r.bakman@icloud.com>
2023-03-12 15:32:40 +0600

    w

--- app/src/main/java/com/MiFy/Buzz/Login/LoginFragment.kt
+++ app/src/main/java/com/MiFy/Buzz/Login/LoginFragment.kt
@@ -5,10 +5,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.MiFy.Buzz.R
 
 class LoginFragment: Fragment() {
+    private var userName: String= ""
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -21,9 +26,19 @@ class LoginFragment: Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         val button = view.findViewById<Button>(R.id.login_btn)
+        checkButtonEnabled()
         button.setOnClickListener {
-            println("Welcome to hell >:D")
+            findNavController().navigate(R.id.action_loginFragment_to_quizFragment)
         }
+        val userTextField = view.findViewById<EditText>(R.id.mail)
+        userTextField.doOnTextChanged { text, start, before, count ->
+            userName=text.toString()
+            checkButtonEnabled()
+        }
+    }
 
+    private fun checkButtonEnabled(){
+        val button = view?.findViewById<Button>(R.id.login_btn)
+        button?.isEnabled = !userName.isEmpty()
     }
 }
